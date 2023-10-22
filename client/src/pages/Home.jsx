17229a99795bb612f9af2d55feedfc17689ce69e
Quynh Nguyen <qtn2@case.edu>
2023-10-22 02:11:54 -0400

    test

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,10 +2,10 @@ import Button from "../components/Button";
 const Home = () => {
     return (
         <div className="">
-            <div className="">
-
+            <div className="home-content">
+                <h1>APP NAME</h1>
             </div>
-            <div className="">
+            <div className="home-button">
                 <Button to="/lesson"> LET'S LEARN </Button>
                 <Button to="/introduction"> INTRODUCTION </Button>
             </div>
